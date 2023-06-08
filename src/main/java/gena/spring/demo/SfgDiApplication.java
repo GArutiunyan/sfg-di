5c713eccae7c6b24813da295e7219a59cffa8369
GArutiunyan <cololor65@gmail.com>
2023-06-08 23:02:47 +0300

    my first controller

--- src/main/java/gena/spring/demo/SfgDiApplication.java
+++ src/main/java/gena/spring/demo/SfgDiApplication.java
@@ -1,13 +1,18 @@
 package gena.spring.demo;
 
+import gena.spring.demo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		MyController myControllerR = (MyController) ctx.getBean("myController");
+		String greeting = myControllerR.sayHello();
+		System.out.println(greeting);
 	}
 
 }
